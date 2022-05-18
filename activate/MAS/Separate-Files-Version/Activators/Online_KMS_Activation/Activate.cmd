d3e47d72bdc739c2c2cd27c5993b67bfa9f2c173
Lazy Git <kashccotton@gmail.com>
2022-05-17 22:05:51 -0700

    Update Activate.cmd

--- activate/MAS/Separate-Files-Version/Activators/Online_KMS_Activation/Activate.cmd
+++ activate/MAS/Separate-Files-Version/Activators/Online_KMS_Activation/Activate.cmd
@@ -250,10 +250,8 @@ mode con: cols=76 lines=33
 echo.
 echo.
 echo.       ______________________________________________________________
-echo.
-echo.              [1] Activate - Windows
-echo.              [2] Activate - Office
-echo.              [3] Activate - All
+echo.          
+echo.              [2] Activate - Office (click)
 echo.
 if defined _tskinstalled call :_color2 %_White% "              [I] Activation Auto-Renewal   " %_Green% "[Installed]"
 if defined _oldtsk       call :_color2 %_White% "              [I] Activation Auto-Renewal   " %_Red% "[Old Installed]"
